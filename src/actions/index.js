93ef1f57659555e0abfd2ccf149d7344b17d462d
jrussel <jriccrussel@gmail.com>
2022-09-02 13:54:16 +0800

    add fetchUser & styling for PostList

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,10 @@ export const fetchPosts = () => async dispatch => {
   
     dispatch({ type: 'FETCH_POSTS', payload: response })
 }
+
+// fetch user by id
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+}
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,11 +8,16 @@ class PostList extends Component {
   }
 
   render() {
+    console.log(this.props.posts)
     return <div>Post List</div>
   }
+}
+
+const mapToStateProps = (state) => {
+  return { posts: state.posts }
 }
 
 // connect so para naa tay access sa mga state like fetchPosts
 // connect(null, { fetchPosts: fetchPosts }) is equivalent to connect(mapToStateProps, mapDispatchToProps)
 // mapToStateProps - read or access sa state, mapDispatchToProps - we use to access our dispatch function or pag access sa dispatch action 
-export default connect(null, { fetchPosts })(PostList)
+export default connect(mapToStateProps, { fetchPosts })(PostList)
